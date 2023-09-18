595a8771b1a596a8331ea327732dfcd5133a032a
Anton <anton@Antons-MacBook-Air.local>
2023-09-18 21:16:28 +0800

    add dependency

--- producer/src/sync.module.ts
+++ producer/src/sync.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { KafkaBrokerModule } from './kafka/kafka.module';
 import { SyncService } from './sync.service';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   controllers: [],
   exports: [SyncService],
-  imports: [KafkaBrokerModule],
+  imports: [KafkaBrokerModule, ConfigModule.forRoot({ isGlobal: true }),],
   providers: [SyncService]
 })
 export class SyncModule {}
