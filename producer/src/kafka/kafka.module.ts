483d81c2776597c3635ce95b3d8cb455a8047ea0
Anton <anton@Antons-MacBook-Air.local>
2023-09-18 22:35:05 +0800

    providers

--- producer/src/kafka/kafka.module.ts
+++ producer/src/kafka/kafka.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 import { KafkaService } from './kafka.service';
 import { getKafkaConnectionConfig } from './kafka.config';
@@ -8,7 +8,7 @@ import { getKafkaConnectionConfig } from './kafka.config';
 @Module({
   exports: [KafkaService],
   imports: [
-    ConfigService,
+    ConfigModule,
     ClientsModule.registerAsync([
       {
         inject: [ConfigService],
