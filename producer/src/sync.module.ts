4f3552e6d40f153e26f60f8b6a6dbd1b45e0d04c
Anton <anton@Antons-MacBook-Air.local>
2023-09-19 15:27:55 +0800

    providers

--- producer/src/sync.module.ts
+++ producer/src/sync.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { KafkaBrokerModule } from './kafka/kafka.module';
 import { SyncService } from './sync.service';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Module({
   controllers: [],
   exports: [SyncService],
   imports: [ConfigModule.forRoot({ isGlobal: true }), KafkaBrokerModule],
-  providers: [SyncService]
+  providers: [SyncService, ConfigService]
 })
 export class SyncModule {}
